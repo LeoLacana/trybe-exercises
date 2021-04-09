3b5edd357f721baa93c09da498f1cd999dd083a9
LeoLacana <wilson17414@gmail.com>
2021-04-09 10:50:41 -0300

    atualização

---
+++ exercises/Bloco_4/dia_4/exer-cont.js
@@ -0,0 +1,7 @@
+let carro = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
+}
+
+console.log(carro.model);
---
+++ exercises/Bloco_4/dia_4/part-I-exer-1.js
@@ -0,0 +1,17 @@
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: 'Sim',
+};
+
+let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: 'Christmas on Bear Mountain, Dells Four Color Comics #178',
+    nota: 'O último MacPatinhas',
+    recorrente:'Sim',
+};
+
+for (let index in info){
+    console.log(info[index] + ' e ' + info2[index]);
+};
